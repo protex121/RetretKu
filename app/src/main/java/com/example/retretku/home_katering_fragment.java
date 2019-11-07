693e9e75a4caa472e27816a403f034d694eb420c
bluelightning40 <39336921+bluelightning40@users.noreply.github.com>
2019-11-07 11:32:45 +0700

    activity katering dummy

---
+++ app/src/main/java/com/example/retretku/home_katering_fragment.java
@@ -0,0 +1,24 @@
+package com.example.retretku;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class home_katering_fragment extends Fragment {
+    EditText txt_nama, txt_deskripsi;
+    Button btn_add;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.home_katering_fragment,container,false);
+        return v;
+    }
+}
--- app/src/main/java/com/example/retretku/katering.java
+++ app/src/main/java/com/example/retretku/katering.java
@@ -1,14 +1,51 @@
 package com.example.retretku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class katering extends AppCompatActivity {
+    Fragment fr = new home_katering_fragment();
+    BottomNavigationView bnv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_katering);
+
+        bnv = findViewById(R.id.bottomNavigationView);
+        bnv.setOnNavigationItemSelectedListener(bnvListener);
+        openFragment(fr);
     }
+
+    public void openFragment(Fragment frag){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment,frag);
+        ft.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bnvListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch(menuItem.getItemId()){
+                case R.id.menu_home:
+                    fr = new home_katering_fragment();
+                    break;
+                case R.id.menu_add_menu:
+                    fr = new add_menu_katering_fragment();
+                    break;
+                case R.id.menu_add_paket:
+                    fr = new add_paket_katering_fragment();
+                    break;
+            }
+            openFragment(fr);
+            return true;
+        }
+    };
 }
