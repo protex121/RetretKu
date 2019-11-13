14cbd467a7f3429ce5ca2b1519c1ea1b4c4f245d
Bobby Ishak <bobbyishak@yahoo.com>
2019-11-13 22:45:37 +0700

    Tambahan home pengelola
    + tambah icon, perubahan sedikit di calendar detail

---
+++ app/src/main/java/com/example/retretku/HomePengelola.java
@@ -0,0 +1,14 @@
+package com.example.retretku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePengelola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pengelola);
+    }
+}
