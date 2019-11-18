419fbbe0aee6ef582a2bc9ff3b50db4112205c16
bluelightning40 <39336921+bluelightning40@users.noreply.github.com>
2019-11-18 08:10:19 +0700

    Fragment Katering

--- app/src/main/java/com/example/retretku/katering.java
+++ app/src/main/java/com/example/retretku/katering.java
@@ -2,24 +2,45 @@ package com.example.retretku;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class katering extends AppCompatActivity {
     Fragment fr = new home_katering_fragment();
     BottomNavigationView bnv;
+    ImageView ImgSideMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_katering);
 
+        ImgSideMenu = findViewById(R.id.imgsidemenu2);
         bnv = findViewById(R.id.bottomNavigationView);
+
+
+
+        ImgSideMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         bnv.setOnNavigationItemSelectedListener(bnvListener);
         openFragment(fr);
     }
