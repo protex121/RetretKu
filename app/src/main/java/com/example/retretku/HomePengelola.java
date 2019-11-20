c724915f56e6ac77ab722ba0d0db0462e8285986
Bobby Ishak <bobbyishak@yahoo.com>
2019-11-20 19:47:05 +0700

    nambah semua fragment activity pengelola
    + pemindahan kata-kata yang bisa ditranslate ke string.xml, layout semua  di activity pengelola done ,tapi recyclerview BELUM DIBUAT SEMUA DALEMNYA

--- app/src/main/java/com/example/retretku/HomePengelola.java
+++ app/src/main/java/com/example/retretku/HomePengelola.java
@@ -5,14 +5,23 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class HomePengelola extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    TextView nama, tipe, since;
+    ImageView pp;
+    RatingBar ratingbar;
+    RecyclerView rv;
+
 
     public HomePengelola() {
         // Required empty public constructor
@@ -24,6 +33,7 @@ public class HomePengelola extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home_pengelola, container, false);
+
         return v;
     }
 
---
+++ app/src/main/java/com/example/retretku/SewaanPengelola.java
@@ -0,0 +1,32 @@
+package com.example.retretku;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SewaanPengelola extends Fragment {
+
+
+    public SewaanPengelola() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_sewaan_pengelola, container, false);
+        return v;
+    }
+
+}
